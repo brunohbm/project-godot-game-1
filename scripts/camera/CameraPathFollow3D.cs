615ef9a12f3912c85aa623a3545311e10817fe0f
Bruno Henrique <brunohbm23@gmail.com>
2024-01-07 21:17:18 -0300

    Addin textures to test camera movement

---
+++ scripts/camera/CameraPathFollow3D.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class CameraPathFollow3D : PathFollow3D
+{
+	[Export]
+	public float ProgressSpeed = 0.01f;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public override void _PhysicsProcess(double delta) {
+		this.ProgressRatio += (float)(ProgressSpeed * delta);
+	}
+}
