438e4d2e09c19dfa06d460efb69054466b206d44
Bruno Henrique <brunohbm23@gmail.com>
2024-01-14 17:23:21 -0300

    Feature: Adding final camera transition.

--- scripts/camera/CameraPathFollow3D.cs
+++ scripts/camera/CameraPathFollow3D.cs
@@ -7,9 +7,9 @@ using System.Diagnostics;
 public partial class CameraPathFollow3D : PathFollow3D
 {
 	[Export]
-	public float ProgressSpeed = 0.01f;
+	public float InitialProgressSpeed = 0.04f;
 	[Export]
-	public float SlowMotionProgressSpeed = 0.01f;
+	public float SlowMotionProgressSpeed = 0.001f;
 	[Export]
 	public Camera3D MainCamera;
 	[Export]
@@ -18,14 +18,27 @@ public partial class CameraPathFollow3D : PathFollow3D
 	public float FirstCameraRotationSpeed;
 	[Export]
 	public float FirstCameraRotationStop;
+	[Export]
+	public float RunningProgressSpeed = 0.1f;
+	[Export]
+	public float SecondCameraTransitionProgress;
+	[Export]
+	public float SecondCameraRotationSpeed;
+	[Export]
+	public float SecondCameraRotationStop;
+	[Export]
+	public float SecondCameraTransitionSpeed = 0.1f;
+	[Export]
+	public float FallingProgressSpeed = 0.1f;
 
 	private bool HasFinishedFirstCameraTransition = false;
-	public float ActualProgressSpeed = 0.01f;
+	private bool HasFinishedSecondCameraTransition = false;
+	public float ActualProgressSpeed = 0.04f;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		ActualProgressSpeed = ProgressSpeed;
+		ActualProgressSpeed = InitialProgressSpeed;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -42,16 +55,44 @@ public partial class CameraPathFollow3D : PathFollow3D
 		HasFinishedFirstCameraTransition = MainCamera.Transform.Basis.Y.X >= FirstCameraRotationStop;
 	}
 
+	void FallingCameraTransition()
+	{
+		Transform3D transform = MainCamera.Transform;
+		transform.Basis = transform.Basis.Rotated(Vector3.Back, SecondCameraRotationSpeed);
+		MainCamera.Transform = transform;
+
+		HasFinishedSecondCameraTransition = MainCamera.Transform.Basis.Y.X <= SecondCameraRotationStop;
+	}
+
 	void CheckCameraTransition()
 	{
-		if (!HasFinishedFirstCameraTransition && this.ProgressRatio >= FirstCameraTransitionProgress)
+		if (HasFinishedFirstCameraTransition)
+		{
+			if (HasFinishedSecondCameraTransition)
+			{
+				ActualProgressSpeed = FallingProgressSpeed;
+				return;
+			}
+
+			if (this.ProgressRatio >= SecondCameraTransitionProgress)
+			{
+				ActualProgressSpeed = SecondCameraTransitionSpeed;
+				FallingCameraTransition();
+				return;
+			}
+
+			ActualProgressSpeed = RunningProgressSpeed;
+			return;
+		}
+
+		if (this.ProgressRatio >= FirstCameraTransitionProgress)
 		{
 			ActualProgressSpeed = SlowMotionProgressSpeed;
 			GetUpCameraTransition();
 			return;
 		}
 
-		ActualProgressSpeed = ProgressSpeed;
+		ActualProgressSpeed = InitialProgressSpeed;
 	}
 
 	public override void _PhysicsProcess(double delta)
